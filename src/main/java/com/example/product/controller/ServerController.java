5d02cdd628d01d14ca8910dd3f3a516e7dff7eb5
lizw <lzwdev@outlook.com>
2018-04-17 22:50:38 +0800

    增加商品服务端服务，做resetTemplate测试

--- src/main/java/com/example/product/controller/ServerController.java
+++ src/main/java/com/example/product/controller/ServerController.java
@@ -3,6 +3,9 @@ package com.example.product.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 商品服务端，启动两个服务8080, 9080
+ */
 @RestController
 public class ServerController {
 
